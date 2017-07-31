fcafee087359575520cbb011fdfd0d0e0877b3c2
vavachan <varghese.i.am@gmail.com>
2017-07-31 17:50:45 +0530

    insertion working

--- BST.cpp
+++ BST.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<math.h>
 /*
  * Program to implement a binary search tree 
  */
@@ -18,25 +19,69 @@ struct break_point
  * compare between two sites:
  * Two sites are compared using a convention adopted.
  */
-int compare(struct site *p1,struct site *p2)
+int compare(struct site p1,struct site p2)
 {
-	if( p1->y > p2->y )
+	if( p1.y > p2.y )
 		return 1;
-	else if ( (p1->y == p2->y) && ( p1->x > p2->x ) )
+	else if ( (p1.y == p2.y) && ( p1.x > p2.x ) )
 		return 1;
-	else if ( (p1->y == p2->y) && ( p1->x = p2->x ) )
+	else if ( (p1.y == p2.y) && ( p1.x = p2.x ) )
 		return 0;
 	else 
 		return -1;
 }
+
+struct site cal_break_point(struct break_point B,float y)
+{
+	float h=B.p1.y-y;
+	float h1=B.p2.y-y;
+	float x1=B.p2.x-B.p1.x;
+	int flag=compare(B.p1,B.p2);
+	struct site BP;
+////////cout<<B.p1.x<<"\t"<<B.p1.y<<"\n";
+////////cout<<B.p2.x<<"\t"<<B.p2.y<<"\n";
+////////cout<<x1<<"\t"<<h<<"\t"<<h1<<"\n";
+	if(flag==1)
+	{
+		BP.x=(-2.*h*x1+sqrt(pow(2.*h*x1,2)-4.*(h1-h)*((h-h1)*h*h1-h*pow(x1,2))))/(2.*(h1-h))+B.p1.x;
+		if(BP.x<B.p2.x)
+		{
+			BP.y=pow(BP.x,2)/(2.*h)+2.*h/2.;
+			return BP;
+		}
+		else 
+		{
+			BP.x=(-2.*h*x1-sqrt(pow(2.*h*x1,2)-4.*(h1-h)*((h-h1)*h*h1-h*pow(x1,2))))/(2.*(h1-h))+B.p1.x;
+			BP.y=pow(BP.x,2)/(2.*h)+2.*h/2.;
+			return BP;
+		}
+	}
+	else if (flag == -1)
+	{
+		BP.x=(-2.*h*x1+sqrt(pow(2.*h*x1,2)-4.*(h1-h)*((h-h1)*h*h1-h*pow(x1,2))))/(2.*(h1-h))+B.p1.x;
+		if(BP.x>B.p1.x)
+		{
+			BP.y=pow(BP.x,2)/(2.*h)+2.*h/2.;
+			return BP;
+		}
+		else 
+		{
+			BP.x=(-2.*h*x1-sqrt(pow(2.*h*x1,2)-4.*(h1-h)*((h-h1)*h*h1-h*pow(x1,2))))/(2.*(h1-h))+B.p1.x;
+			BP.y=pow(BP.x,2)/(2.*h)+2.*h/2.;
+			return BP;
+		}
+	}
+}
 //node definition 
 struct node 
 {
-	int data=1;
         struct site p;
         struct break_point B;
         struct node *left; 
         struct node *right;
+        struct node *adj_left; 
+        struct node *adj_right;
+	struct node *parent;
 }*root;
 /*
  * Defining a Class called Binary Search Tree
@@ -46,50 +91,69 @@ class BST
 	public:
 		void insert(node *, node *);
 		void display(node *,int);
+		void find(node *,int);
+		void disBeach(node *);
 };
 void BST::insert(node *tree, node *newnode)
 {
 	if(root==NULL)
 	{
 		root = new node;
-		root->data=newnode->data;	
+		root->p=newnode->p;	
 		root->left=NULL;
 		root->right=NULL;
+		root->adj_left=NULL;
+		root->adj_right=NULL;
+		root->parent=NULL;
 		return;
 	}
-	if(tree->data>newnode->data)
+	if( tree->left==NULL && tree->right == NULL )
 	{
-		if(tree->left==NULL)
-		{
-			tree->left = new node;
-			tree->left->data=newnode->data;
-			tree->left->left=NULL;
-			tree->left->right=NULL;
-			return;
-		}
-		else
-		{
-			insert(tree->left,newnode);
-		}
-	}
-	else if(tree->data<newnode->data)
-	{
-		if(tree->right==NULL)
-		{
-			tree->right = new node;
-			tree->right->data=newnode->data;
-			tree->right->left=NULL;
-			tree->right->right=NULL;
-			return;
-		}
-		else
-		{
-			insert(tree->right,newnode);
-		}
+		tree->left=new node;
+		tree->left->parent=tree;
+		//Moving the node down
+		tree->left->p=tree->p;
+		//assigning the left and right adjacent nodes
+		tree->left->adj_left=tree->adj_left;
+//		tree->left->adj_right=newnode;
+		//No further branches 
+		tree->left->left=NULL;
+		tree->left->right=NULL;
+		//creating a breakpoint
+		tree->B.p1=tree->p;
+		tree->B.p2=newnode->p;
+		tree->right=new node;
+		tree->right->parent=tree;
+		//the right node also a breakpoint
+		tree->right->B.p1=newnode->p;
+		tree->right->B.p2=tree->p;
+		tree->right->left=new node;
+		tree->right->right=new node;
+		tree->right->left->parent=tree->right;
+		tree->right->right->parent=tree->right;
+		tree->right->left->p=newnode->p;
+		tree->right->left->adj_left=tree->left;	
+  		tree->left->adj_right=tree->right->left;
+		tree->right->right->p=tree->p;
+		tree->right->left->adj_right=tree->right->right;	
+		tree->right->right->adj_left=tree->right->left;	
+		tree->right->right->adj_right=tree->adj_right;	
+		tree->right->left->left=NULL;
+		tree->right->left->right=NULL;
+		tree->right->right->left=NULL;
+		tree->right->right->right=NULL;
+		return;
 	}
 	else
-		cout<<"file already exists\n";
-	return;
+	{
+		struct site bp;
+		bp=cal_break_point(tree->B,newnode->p.y);
+	//	cout<<bp.x<<"\n";
+		if(newnode->p.x<bp.x)
+			insert(tree->left,newnode);
+		else
+			insert(tree->right,newnode);
+	}
 }
 void BST::display(node *tree,int level=0)
 {
@@ -103,11 +167,27 @@ void BST::display(node *tree,int level=0)
 	{
 		cout<<"      ";
 	}
-	cout<<tree->data<<"\n";
+	if( tree->left!=NULL && tree->right != NULL )
+		cout<<level<<"(X,X)\n";
+	else
+		cout<<"("<<tree->p.x<<","<<tree->p.y<<")\n";
 	if(tree->left!=NULL)
 	{
 		display(tree->left,level+1);
 	}
+}
+void BST::disBeach(node *tree)
+{
+	struct site bp;
+	if( tree->left==NULL && tree->right == NULL )
+	{
+		cout<<"("<<tree->p.x<<","<<tree->p.y<<")\n";
+	////////bp=cal_break_point(tree->parent->B,1.);	
+	////////cout<<bp.x<<"\n";
+		disBeach(tree->adj_right);
+	}
+	else 
+	disBeach(tree->left);
 }
 int main()
 {
@@ -115,22 +195,20 @@ int main()
 	node *temp;
 	temp = new node;
 //	cout<<temp->data<<"\n";
-////////cout<<"hello\n";
-        temp->data=2;
+        temp->p.x=1.;
+	temp->p.y=5.;
 ////////cout<<"hello\n";
 //	temp->p.x=1.0;
 //	temp->p.y=1.0;
         bst.insert(root,temp);
-        temp->data=3;
+        temp->p.x=0.;
+	temp->p.y=4.;
+        cout<<"hello\n";
         bst.insert(root,temp);
-        temp->data=1;
-        bst.insert(root,temp);
-        temp->data=4;
-        bst.insert(root,temp);
-        temp->data=7;
-        bst.insert(root,temp);
-        temp->data=6;
+        temp->p.x=-1.;
+	temp->p.y=1.;
         bst.insert(root,temp);
         bst.display(root);
+	bst.disBeach(root);
 	return 0;
 }
